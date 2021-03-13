fe299dc861da41dd0c1c7bd48c5250664cef4fb0
farepule <faruk.puska@gmail.com>
2021-03-13 08:00:18 +0100

    Strings

---
+++ ex03/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  //Only change code below this line
+  var mystring = "FirstLine\n\tSecondLine\nThirdeLine";
+  //Only change code above this line
+  return my;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  //Only change code below this line
+  var mystring = "This is the first string. " + "This is the second string.";
+  //Only change code above this line
+  return mystring;
+}
+
+console.log(main());
+module.exports = main;
